d2987ee6b2d25f6752ef190d2a18a62064099f69
Hideaki Tanabe <tanablog@gmail.com>
2019-03-03 22:37:21 +0900

    update aar

--- Assets/Sphere.cs
+++ Assets/Sphere.cs
@@ -15,29 +15,43 @@ public class Sphere : MonoBehaviour
     {
         if (Application.isEditor) {
             if (Input.GetMouseButtonDown(0)) {
-                Debug.Log("クリックした瞬間");
+                Debug.Log("clicked");
             }
         } else {
             if (Input.GetMouseButtonDown(0)) {
-                Debug.Log("クリックした瞬間");
-                ShowDialog();
+                Debug.Log("tapped");
+                //ShowDialog();
+                bool result = IsGooglePlayServicesAvailable();
+                Debug.Log(result);
             }
         }
     }
 
-    void ShowDialog() {
+    void ShowDialog()
+    {
 #if UNITY_ANDROID
-        AndroidJavaClass nativeDialog = new AndroidJavaClass("com.kaihatsubu.android.safetynetapimodule.SafetyNetAPIUtil");
+        AndroidJavaClass safetyNetAPIUtil = new AndroidJavaClass("com.kaihatsubu.android.safetynetapimodule.SafetyNetAPIUtil");
         AndroidJavaClass unityPlayer = new AndroidJavaClass("com.unity3d.player.UnityPlayer");
         AndroidJavaObject context = unityPlayer.GetStatic<AndroidJavaObject>("currentActivity");
-        context.Call("runOnUiThread", new AndroidJavaRunnable(() => {
-            nativeDialog.CallStatic(
+        context.Call("runOnUiThread", new AndroidJavaRunnable(() =>
+        {
+            safetyNetAPIUtil.CallStatic(
                 "showMessage",
                 context,
                 "test",
                 "this is test dialog"
             );
         }));
+#endif
+    }
+
+    bool IsGooglePlayServicesAvailable() {
+#if UNITY_ANDROID
+        AndroidJavaClass safetyNetAPIUtil = new AndroidJavaClass("com.kaihatsubu.android.safetynetapimodule.SafetyNetAPIUtil");
+        AndroidJavaClass unityPlayer = new AndroidJavaClass("com.unity3d.player.UnityPlayer");
+        AndroidJavaObject context = unityPlayer.GetStatic<AndroidJavaObject>("currentActivity");
+        bool result = safetyNetAPIUtil.CallStatic<bool>("isGooglePlayServicesAvailable", context);
+        return result;
 #endif
     }
 }
